01852b2bf23122884d43044e1149f5d1b7e20627
João Vitor <vtrdll@DESKTOP-5VD24SI.localdomain>
2025-10-08 23:14:35 -0300

    spuer

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,27 @@
 
+import os
+from django.contrib.auth.models import User
+from django.db.utils import OperationalError
 
 
 
 from .celery import app as celery_app
 
 __all__ = ('celery_app',)
+
+
+
+def create_default_superuser():
+    try:
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='admin',
+                email='admin@meusite.com',
+                password='Curitiba@2'
+            )
+            print("Superuser criado!")
+    except OperationalError:
+        # Isso evita erro se o banco ainda não estiver migrado
+        pass
+
+create_default_superuser()
