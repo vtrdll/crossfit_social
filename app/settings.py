7467a19f5aa68a97c7d737c10981da273e81062b
João Vitor <vtrdll@DESKTOP-5VD24SI.localdomain>
2025-10-08 21:44:34 -0300

    alteracao csrf

--- app/settings.py
+++ app/settings.py
@@ -36,7 +36,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 # HOSTS & CSRF
 # ------------------------------
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
-CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+CSRF_TRUSTED_ORIGINS = ["https://crossfitsocial-social.up.railway.app", "https://127.0.0.1:8000"]
 
 # ------------------------------
 # DATABASE
